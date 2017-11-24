40e295c84d8e42d205307c35d8345238e45cdba6
Thiago de Paula Beserra <thiago.beserra@valecard.com.br>
2017-11-24 18:54:47 -0200

    alteracao no objeto de banco contato

--- tables/05-Table_contato.sql
+++ tables/05-Table_contato.sql
@@ -2,11 +2,18 @@ use pcv_db;
 
 create table contato (
 	id bigint primary key auto_increment,
+    data_cadastro datetime,
+    data_atualizacao datetime,
 	nome varchar(30),
 	sobrenome varchar(50),
 	cpf varchar(14),
-        idade int,
-        data_nasc datetime,
+    idade int,
+    data_nasc datetime,
+	telefone varchar (18),
+	celular varchar (18),
+    email varchar (100),
+    profissao varchar (50),
+    salario varchar (8),
 	endereco_id bigint not null,
 	constraint fk_endereco_id foreign key (endereco_id) references endereco(id)
 ) engine=InnoDB;
